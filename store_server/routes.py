7cf3c47166686771f6b75924ff2a1e24ae21e14f
lambrosopos <lambrosopos@gmail.com>
2021-10-12 22:47:52 +0900

    add python-dotenv for env management

--- store_server/routes.py
+++ store_server/routes.py
@@ -7,7 +7,15 @@ main = Blueprint('main', __name__, url_prefix='/')
 def index():
     return 'OK', 200
 
-@main.route('/message')
-def receive_message():
+@main.route('/webhook', methods=['POST'])
+def receive_webhook():
+    key = request.GET.get('key', '')
+
+    if not key:
+        return 'Invalid Request', 403
+
+    if key != 'ef47e2f3d598da95dd183c059a0baedf':
+        return 'Invalid Credentials', 403
+
     return 'OK', 200
 
